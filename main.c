83133354a7abb572c747954347ef5489ba2f2a9a
masaki <git@kittens.army>
2022-11-04 14:50:55 +0900

    parse links between rooms

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 10:22:17 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/04 14:40:18 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/11/04 14:50:13 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ int		parse_number(char** str)
 {
 	int		nb;
 
-	while (ft_isspace(**str))
+	while (ft_isspace(**str) || **str == '-')
 		(*str)++;
 	nb = 0;
 	while (ft_isdigit(**str))
@@ -89,7 +89,7 @@ int		parse_number(char** str)
 void	parse_room(char* line, t_map* map)
 {
 	t_room*		new_room;
-	t_list*		new_link;
+	t_list*		lst_elem;
 	
 	new_room = malloc(sizeof(t_room));
 	if (new_room == NULL)
@@ -98,18 +98,31 @@ void	parse_room(char* line, t_map* map)
 	new_room->x = parse_number(&line);
 	new_room->y = parse_number(&line);
 
-	new_link = ft_lstnew(new_room);
-	if (new_link == NULL)
+	lst_elem = ft_lstnew(new_room);
+	if (lst_elem == NULL)
 		printf("malloc err %d\n", MALLOC_ERR);
-	ft_lstadd_back(&map->rooms, new_link);
+	ft_lstadd_back(&map->rooms, lst_elem);
 
 	printf("ROOM no %d, x = %d, y = %d\n", new_room->id, new_room->x, new_room->y);
 }
 
 void	parse_link(char* line, t_map* map)
 {
-	(void)line;
-	(void)map;
+	t_link*		new_link;
+	t_list*		lst_elem;
+	
+	new_link = malloc(sizeof(t_link));
+	if (new_link == NULL)
+		printf("malloc err %d\n", MALLOC_ERR);
+	new_link->room1 = parse_number(&line);
+	new_link->room1 = parse_number(&line);
+
+	lst_elem = ft_lstnew(new_link);
+	if (lst_elem == NULL)
+		printf("malloc err %d\n", MALLOC_ERR);
+	ft_lstadd_back(&map->rooms, lst_elem);
+
+	printf("LINK between rooms %d and %d\n", new_link->room1, new_link->room2);
 }
 
 char*	digit(t_state_machine* machine, char* line)
