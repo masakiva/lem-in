e5b76be7b930227bea6d17b3afa33fb5595a5a8d
Tagawa Kodai <tkodai@c1r28s1.42tokyo.jp>
2022-11-04 16:49:22 +0900

    Add: store rooms in array

--- solve.c
+++ solve.c
@@ -6,16 +6,48 @@
 /*   By: tkodai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:33:11 by tkodai            #+#    #+#             */
-/*   Updated: 2022/11/04 15:39:16 by tkodai           ###   ########.fr       */
+/*   Updated: 2022/11/04 16:46:44 by tkodai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem-in.h"
 #include "solve.h"
+#include "libft.h"
 #include <stdio.h>
+
+void	parse_data(t_solve *s, t_map *map)
+{
+	s->rooms_num = ft_lstsize(map->rooms);
+	printf("room num %d\n", s->rooms_num);
+	
+	s->all_links_num = ft_lstsize(map->links);
+	printf("links num %d\n", s->all_links_num);
+
+}
+
+void	create_rooms(t_solve *s, t_map *map)
+{
+	t_solve_room	new_room;
+	int				i;
+	t_list			*room_ptr;
+
+	s->rooms = malloc(sizeof(t_solve_room) * (s->rooms_num));
+	i = 0;
+	room_ptr = map->rooms;
+	while (i < s->rooms_num)
+	{
+		new_room.id = i;
+		new_room.x = ((t_room *)room_ptr->content)->x; 
+		new_room.y = ((t_room *)room_ptr->content)->y;
+		i++;
+		room_ptr = room_ptr->next;
+	}
+}
 
 void	solve(t_map *map)
 {
-	printf("called solve\n");
-	(void)map;
+	t_solve	s;
+
+	parse_data(&s, map);
+	create_rooms(&s, map);
 }
--- solve.h
+++ solve.h
@@ -6,7 +6,7 @@
 /*   By: tkodai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:35:31 by tkodai            #+#    #+#             */
-/*   Updated: 2022/11/04 15:42:19 by tkodai           ###   ########.fr       */
+/*   Updated: 2022/11/04 16:41:35 by tkodai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,26 @@
 # define SOLVE_H
 
 # include "lem-in.h"
+# include <stdlib.h>
+
+#define		ARRY_END	-42
+
+typedef	struct	s_sovle_room
+{
+	int		id;
+	int		x;
+	int		y;
+	int		*links;
+	int		links_num;
+}				t_solve_room;
+
+typedef struct	s_solve
+{
+	t_solve_room	*rooms;
+	int				rooms_num;
+	int				all_links_num;
+
+}				t_solve;
 
 void	solve(t_map *m);
 void	print_data(t_map *m);
