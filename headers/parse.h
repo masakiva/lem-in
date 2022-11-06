403e6647ec1d86f8fa65a5515401d8cee50fb198
masaki <git@kittens.army>
2022-11-06 18:56:53 +0900

    dispatch parsing functions in three files

--- headers/parse.h
+++ headers/parse.h
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 10:22:54 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/06 18:26:07 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/11/06 18:53:59 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,5 +70,19 @@ typedef struct	s_state_machine
 typedef char*	(*t_parse)(t_state_machine *, char *);
 
 int		parse_line(t_map* map);
+
+
+// state machine states
+char*	character(t_state_machine* machine, char* line);
+char*	roomname(t_state_machine* machine, char* line);
+char*	hash(t_state_machine* machine, char* line);
+char*	double_hash(t_state_machine* machine, char* line);
+char*	start_end_line(t_state_machine* machine, char* line);
+
+// utils
+int		isroom(char* line);
+int		islink(char* line);
+void	parse_room(char* line, t_map* map);
+void	parse_link(char* line, t_map* map);
 
 #endif // PARSE_H
---
+++ sources/parse_states.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_states.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 18:46:30 by mvidal-a          #+#    #+#             */
+/*   Updated: 2022/11/06 18:51:35 by mvidal-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.h"
+#include <stdio.h>
+
+char*	character(t_state_machine* machine, char* line)
+{
+	if (*line == '#')
+	{
+		machine->state = HASH;
+		line++;
+	}
+	else if (*line == '\0')
+		machine->state = END;
+	else if (*line == 'L')
+	{
+		printf("input err %d\n", INPUT_ERR);
+		machine->state = END;
+	}
+	else
+		machine->state = ROOMNAME;
+	return (line);
+}
+char*	roomname(t_state_machine* machine, char* line)
+{
+	if (isroom(line))
+		parse_room(line, machine->map);
+	else if (islink(line))
+		parse_link(line, machine->map);
+	else
+		printf("input err %d\n", INPUT_ERR);
+	machine->state = END;
+	return (line);
+}
+
+char*	hash(t_state_machine* machine, char* line)
+{
+	printf("hash\n");
+	if (*line == '#')
+	{
+		machine->state = DOUBLE_HASH;
+		line++;
+	}
+	else // it's a comment
+		machine->state = END;
+	return (line);
+}
+
+char*	double_hash(t_state_machine* machine, char* line)
+{
+	printf("double hash\n");
+	if (ft_strcmp(line, "start") == 0)
+		machine->map->start_flag = TRUE;
+	else if (ft_strcmp(line, "end") == 0)
+		machine->map->end_flag = TRUE;
+	else
+		printf("input err %d\n", INPUT_ERR);
+	machine->state = END;
+	return (line);
+}
+
+char*	start_end_line(t_state_machine* machine, char* line)
+{
+	if (isroom(line))
+	{
+		parse_room(line, machine->map);
+		if (machine->map->start_flag == TRUE)
+		{
+			printf("STARTTTT\n");
+			machine->map->start =
+				((t_room *)ft_lstlast(machine->map->rooms)->content)->name;
+			machine->map->start_flag = FALSE;
+		}
+		if (machine->map->end_flag == TRUE)
+		{
+			printf("ENDDDD\n");
+			machine->map->end =
+				((t_room *)ft_lstlast(machine->map->rooms)->content)->name;
+			machine->map->end_flag = FALSE;
+		}
+	}
+	else
+		printf("input err %d\n", INPUT_ERR);
+	machine->state = END;
+	return (line);
+}
+
