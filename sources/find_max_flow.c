4a9b73a57c871fcc7b87a8b567cebc7d61939c75
Tagawa Kodai <tkodai@c1r28s1.42tokyo.jp>
2022-11-08 11:33:01 +0900

    Add: stash

--- sources/find_max_flow.c
+++ sources/find_max_flow.c
@@ -51,6 +51,42 @@ int		flow_dfs(int from, int end, int cap, t_ek_graph *graph)
 	return 0;
 }
 
+//再帰的に帰るならmallocして詰めて帰る。
+void	follow_root_recurse(int current_id, int end_id, t_ek_graph *graph)
+{
+	int i = 0;
+
+	if (current_id == end_id)
+	{
+		printf("end\n");
+		return ;
+	}
+
+	int flag;
+
+	while (i < graph->nodes[current_id].edges_size)
+	{
+		flag = 1;
+		if (current_id % 2 == 1)
+
+		if (graph->nodes[current_id].edges[i].cap == 0)
+		{
+			printf("%d\n", current_id);
+			follow_root_recurse(graph->nodes[current_id].edges[i].to, end_id, graph);
+		}
+		i++;
+	}
+}
+
+void	follow_root(t_map *map, t_solve *s, t_ek_graph *graph)
+{
+	(void)map;
+	(void)s;
+	
+	printf("start\n");
+	follow_root_recurse(graph->start_output_id, graph->end_input_id, graph);
+}
+
 void	find_max_flow(t_map *map, t_solve *s, t_ek_graph *graph)
 {
 	int		ret;
@@ -64,6 +100,7 @@ void	find_max_flow(t_map *map, t_solve *s, t_ek_graph *graph)
 		used_set_zero(graph);
 		ret = flow_dfs(graph->start_output_id, graph->end_input_id, 1, graph);
 
+		follow_root(map, s, graph);
 		printf("dfs %d\n", ret);
 		if (ret == 0)
 			break;
