01166c310f8ccf512beb8d1bad834b8b9556488b
masaki <git@kittens.army>
2022-11-08 16:51:21 +0900

    change a bit the way of finding duplicates

--- sources/parse_utils.c
+++ sources/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 18:51:23 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/08 16:40:38 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/11/08 16:49:19 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,8 +124,7 @@ static t_bool	find_dup_roomname(t_list* rooms, char* name_to_compare)
 	while (rooms != NULL)
 	{
 		cur_room = (t_room *)rooms->content;
-		if (cur_room->name != name_to_compare
-				&& ft_strcmp(cur_room->name, name_to_compare) == 0)
+		if (ft_strcmp(cur_room->name, name_to_compare) == 0)
 			return (TRUE);
 		rooms = rooms->next;
 	}
@@ -139,8 +138,7 @@ static t_bool	find_dup_roomcoord(t_list* rooms, t_room* room_to_compare)
 	while (rooms != NULL)
 	{
 		cur_room = (t_room *)rooms->content;
-		if (cur_room != room_to_compare
-				&& cur_room->x == room_to_compare->x
+		if (cur_room->x == room_to_compare->x
 				&& cur_room->y == room_to_compare->y)
 			return (TRUE);
 		rooms = rooms->next;
@@ -156,9 +154,13 @@ void	parse_room(char* line, t_map* map)
 	new_room = malloc(sizeof(t_room));
 	if (new_room == NULL)
 		error_exit(MALLOC_ERR);
+
 	new_room->name = parse_roomname(&line);
 	if (new_room->name == NULL)
 		error_exit(MALLOC_ERR);
+	if (find_dup_roomname(map->rooms, new_room->name))
+		error_exit(DUP_ROOMNAME);
+
 	if (ft_isint(line) == FALSE)
 		error_exit(COORD_INT_OVERFLOW);
 	new_room->x = ft_atoi(line);
@@ -167,18 +169,13 @@ void	parse_room(char* line, t_map* map)
 		error_exit(COORD_INT_OVERFLOW);
 	new_room->y = ft_atoi(line);
 	line = skip_int(line);
+	if (find_dup_roomcoord(map->rooms, new_room))
+		error_exit(DUP_ROOMCOORD);
 
 	lst_elem = ft_lstnew(new_room);
 	if (lst_elem == NULL)
 		error_exit(MALLOC_ERR);
 	ft_lstadd_back(&map->rooms, lst_elem);
-
-	if (find_dup_roomname(map->rooms,
-				((t_room *)ft_lstlast(map->rooms)->content)->name))
-		error_exit(DUP_ROOMNAME);
-	if (find_dup_roomcoord(map->rooms,
-				(t_room *)ft_lstlast(map->rooms)->content))
-		error_exit(DUP_ROOMCOORD);
 
 	printf("[ROOM] name: \"%s\", x: %d, y: %d\n", new_room->name, new_room->x, new_room->y);
 }
@@ -204,15 +201,12 @@ static t_bool	find_dup_link(t_list* links, t_link* link_to_compare)
 	while (links != NULL)
 	{
 		cur_link = (t_link *)links->content;
-		if (cur_link != link_to_compare)
-		{
-			if (ft_strcmp(cur_link->room1, link_to_compare->room1) == 0
-					&& ft_strcmp(cur_link->room2, link_to_compare->room2) == 0)
-				return (TRUE);
-			else if (ft_strcmp(cur_link->room1, link_to_compare->room2) == 0
-					&& ft_strcmp(cur_link->room2, link_to_compare->room1) == 0)
-				return (TRUE);
-		}
+		if (ft_strcmp(cur_link->room1, link_to_compare->room1) == 0
+				&& ft_strcmp(cur_link->room2, link_to_compare->room2) == 0)
+			return (TRUE);
+		else if (ft_strcmp(cur_link->room1, link_to_compare->room2) == 0
+				&& ft_strcmp(cur_link->room2, link_to_compare->room1) == 0)
+			return (TRUE);
 		links = links->next;
 	}
 	return (FALSE);
@@ -226,6 +220,7 @@ void	parse_link(char* line, t_map* map)
 	new_link = malloc(sizeof(t_link));
 	if (new_link == NULL)
 		error_exit(MALLOC_ERR);
+
 	new_link->room1 = parse_roomname(&line);
 	if (new_link->room1 == NULL)
 		error_exit(MALLOC_ERR);
@@ -240,14 +235,13 @@ void	parse_link(char* line, t_map* map)
 		error_exit(LINK_SAME_ROOMNAMES);
 	if (find_room(new_link->room2, map->rooms) == FALSE)
 		error_exit(LINK_UNKNOWN_ROOMNAME);
+	if (find_dup_link(map->links, new_link))
+		error_exit(DUP_LINK);
 
 	lst_elem = ft_lstnew(new_link);
 	if (lst_elem == NULL)
 		error_exit(MALLOC_ERR);
 	ft_lstadd_back(&map->links, lst_elem);
-
-	if (find_dup_link(map->links, (t_link *)ft_lstlast(map->links)->content))
-		error_exit(DUP_LINK);
 
 	printf("[LINK] between rooms \"%s\" and \"%s\"\n", new_link->room1, new_link->room2);
 }
