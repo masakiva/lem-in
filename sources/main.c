67d8eacf92ad5d8b0cd76299e122a4aaf5c55960
masaki <git@kittens.army>
2022-11-09 12:15:51 +0900

    write help message

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:46:12 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/09 11:28:22 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/11/09 12:15:06 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
  
@@ -16,25 +16,25 @@
 
 int		main(int argc, char** argv)
 {
-	int		ret;
-	t_map	map;
 	t_byte	options;
+	t_map	map;
+	int		ret;
 
 	if (argc > 1)
 	{
 		options = retrieve_options(argc, argv, ARG_OPTIONS);
 		if (options & HELP_OPTION)
 		{
-			ft_putstr_fd("[lem-in] usage: \n", STDOUT_FILENO);
+			ft_putstr_fd(HELP_MSG, STDOUT_FILENO);
 			return (EXIT_SUCCESS);
 		}
 	}
+
 	ft_bzero(&map, sizeof(t_map));
+
 	ret = 1;
 	while (ret == 1)
-	{
 		ret = parse_line(&map);
-	}
 
 	solve(&map);
 	
