3c5e71bde2ae17824978c9df4e54f8a268273422
masaki <git@kittens.army>
2022-11-09 11:33:22 +0900

    add help option support

--- sources/main.c
+++ sources/main.c
@@ -6,19 +6,29 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 17:46:12 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/08 11:55:38 by tkodai           ###   ########.fr       */
+/*   Updated: 2022/11/09 11:28:22 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
  
 #include "parse.h"
 #include "solve.h"
-#include <stdio.h>
+#include <unistd.h>
 
-int		main(void)
+int		main(int argc, char** argv)
 {
 	int		ret;
 	t_map	map;
+	t_byte	options;
 
+	if (argc > 1)
+	{
+		options = retrieve_options(argc, argv, ARG_OPTIONS);
+		if (options & HELP_OPTION)
+		{
+			ft_putstr_fd("[lem-in] usage: \n", STDOUT_FILENO);
+			return (EXIT_SUCCESS);
+		}
+	}
 	ft_bzero(&map, sizeof(t_map));
 	ret = 1;
 	while (ret == 1)
